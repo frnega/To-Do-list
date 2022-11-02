5a3ac4e5319d4d8e6c04705fd6d5b3d4abc25b43
frehiywot nega <fr.nega@gmail.com>
2022-11-02 21:26:16 +0300

    fix to eslibt errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import './style.css';
-import List from './listt.js';
+import List from './list.js'
 const todoList = new List();
 todoList.display();
+// adds new Activity
 document.querySelector('#add-task').addEventListener('submit', (e) => {
   e.preventDefault();
   const activity = e.target.elements.activity.value;
   todoList.addActivity(activity);
   e.target.reset();
 });
+
+//  clears completed Activities
 document.querySelector('.clear-completed').addEventListener('click', () => {
   todoList.clearCompleted();
 });
+
+//  clears all handler
 document.querySelector('#delete-all').addEventListener('click', () => {
   todoList.clearAll();
 });
